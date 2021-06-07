66e549fa361901b7e05e7d72f382949eb9a09685
cukier91 <konarowski.bartosz@gmail.com>
2021-06-07 17:52:10 +0200

    Modele utworzone

--- web_app/models.py
+++ web_app/models.py
@@ -1,5 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Type(models.TextChoices):
+    foundation = "Fundacja", "Fundacja"
+    non_gov_organization = "Organizacja pozarządowa", "Organizacja pozarządowa"
+    local_donation = "Zbiórka lokalna", "Zbiórka lokalna"
 
 
 class CategoryModel(models.Model):
     name = models.CharField(max_length=500)
+
+
+class InstitutionModel(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(null=True)
+    type = models.CharField(max_length=50, choices=Type.choices, default=Type.foundation)
+    category = models.ManyToManyField(CategoryModel)
+
+
+class DonationModel(models.Model):
+    quantity = models.PositiveIntegerField()
+    categories = models.ManyToManyField(CategoryModel)
+    institution = models.ForeignKey(InstitutionModel, on_delete=models.CASCADE)
+    address = models.CharField(max_length=500)
+    phone_no = models.CharField(max_length=20)
+    city = models.CharField(max_length=300)
+    zip_code = models.CharField(max_length=20)
+    pick_up_date = models.DateField(null=False)
+    pick_up_time = models.TimeField(null=False)
+    pick_up_comment = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
