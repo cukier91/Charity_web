ecb34e86ef897dc3a1e03848c0472cd3cb2a1117
cukier91 <konarowski.bartosz@gmail.com>
2021-06-09 19:45:32 +0200

    Poprawiona wydajność

--- web_app/views.py
+++ web_app/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from web_app.models import *
-from django.db.models import Count, Q
+from django.db.models import Count, Sum
 
 
 class LandingPage(View):
     def get(self, request, *args, **kwargs):
-        all_bags = DonationModel.objects.all()
-        total_bags = 0
-        for bag in all_bags:
-            total_bags += bag.quantity
-        institutions = InstitutionModel.objects.all()
-        elements_to_count = 0
-        for institution in institutions:
-            if len(DonationModel.objects.filter(quantity__gt=0, institution_id=institution.id)) is not 0:
-                elements_to_count += 1
+        bags = DonationModel.objects.aggregate(
+            total=Sum('quantity'),
+            institution=Count('pk')
+        ),
         context = {
-            'institution': elements_to_count,
-            'total': total_bags,
+            'total': bags[0]['total'],
+            'institution': bags[0]['institution'],
+
         }
+
+
         return render(request, 'web_app/index.html', context)
 
 
