5acc433f53ba0f439a9cbf9bafde623c9415a96d
cukier91 <konarowski.bartosz@gmail.com>
2021-06-13 14:07:04 +0200

    Działa tworzenie użytkowników na podstawie e-maila

--- web_app/forms.py
+++ web_app/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from web_app.models import User
 
 
 class CreateUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2', 'first_name', 'last_name', 'username']
+        fields = ['email', 'password1', 'password2', 'first_name', 'last_name']
         widgets = {
             'first_name': forms.TextInput(attrs={'placeholder': "Podaj imię.. "}),
             'last_name': forms.TextInput(attrs={'placeholder': "Podaj nazwisko.."}),
             'email': forms.EmailInput(attrs={'placeholder': "e-mail.."}),
-            'username': forms.TextInput(attrs={'placeholder': "Podaj login.."}),
 
         }
+
