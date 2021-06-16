2e2f0f5a5c6eba5c6d1a94c52cdffd882a6f5717
cukier91 <konarowski.bartosz@gmail.com>
2021-06-16 22:56:12 +0200

    Zmiany w panelu administratora, możliwość tworzenia na bazie modeli nowych rekordów w bazie danych

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
-from .models import User
+from .models import User, InstitutionModel, DonationModel, CategoryModel
 from django.conf import settings
 
 
@@ -25,3 +25,16 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+
+@admin.register(InstitutionModel)
+class InstitutionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DonationModel)
+class DonationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
